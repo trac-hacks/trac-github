549325d4e10e46ccb56428f4e8328afb69e94a15
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-05-19 22:12:28 +0200

    Bump version to 1.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GitHubPlugin',
-    version='0.9',
+    version='1.0',
     author='Aymeric Augustin',
     author_email='aymeric.augustin@m4x.org',
     url='https://github.com/aaugustin/trac-github',
