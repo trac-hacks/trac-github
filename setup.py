eccc5bac23bf7abd5e29505e5a422d4ad8b7d4d1
Baptiste Mispelon <bmispelon@gmail.com>
2024-02-03 11:14:28 +0100

    Added Python3 classifiers in setup.py

--- setup.py
+++ setup.py
@@ -30,4 +30,16 @@ setup(
     ]},
     test_suite='runtests',
     tests_require='lxml',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Framework :: Trac',
+    ],
 )
